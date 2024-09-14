e8e4105fa150bb1b2d25c237e12a4de4632c6eff
alexsuarez33 <alex.suarez0129@gmail.com>
2024-09-13 22:47:05 -0400

    Finished child processes, added power of 2 function, removed area function, added parent wait function and final message

--- alexandersuarez.c
+++ alexandersuarez.c
@@ -26,9 +26,28 @@ int isPrime(int num){
     return 1; 
 }
 
-//Calculate area of a square
-int squareArea(int num){
-    return num * num;
+//Print primes
+void printPrimes(int num){
+    for (int i = 2; i <= num; i++){
+        if (isPrime(i)){
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+}
+
+//Calculate two to the power of n
+int twoPower(int n){
+    if (n == 0)
+        return 1;
+    else {
+        return 2 * twoPower(n - 1);
+    }
+}
+
+//Calculate perimiter of a rectangle
+int perimiter(int l, int w){
+    return l * 2 + w * 2;
 }
 
 
@@ -54,9 +73,44 @@ int main() {
             exit(EXIT_FAILURE);
         }
         else if (child_pid == 0){
-            
+            printf("Child %d (PID: %d) created.\n", i, getpid());
+
+            if (i == 1){
+                //Compute factorial of a random number between 1 and 8
+                int factor = rand() % 9;
+                printf("Child %d (PID: %d) is computing the factorial of %d.\n", i, getpid(), factor);
+                int result = factorial(factor);
+                printf("Child %d (PID: %d) completed its task of finding the factorial of %d = %d", i, getpid(), factor, result);
+            }
+            else if (i == 2){
+                //print prime numbers up to 20
+                printf("Child %d (PID: %d) is computing prime numbers up to 20.\n", i, getpid());
+                printf("Child %d (PID: %d) finished computing, prime numbers up to 20: ", i, getpid());
+                printPrimes(20);
+            }
+            else if (i == 3){
+                int power = rand() % 10;
+                printf("Child %d (PID: %d) is computing two to the power of %d. \n", i, getpid(), power);
+                int result = twoPower(power);
+                printf("Child %d (PID: %d) finished computing 2 to the power of %d: %d.\n", i, getpid(), power, result);
+            }
+            else if (i == 4){
+                int length = rand() % 100;
+                int width = rand() % 50;
+                printf("Child %d (PID: %d) is calculating the perimiter of a rectangle of length %d, and width %d.\n", i, getpid(), length, width);
+                int result = perimiter(length, width);
+                printf("Child %d (PID: %d) finished its task, resulting perimiter: %d.\n", i, getpid(), result);
+            }
+
+            exit(0);
         }
     }
+
+    for (int i = 1; i <= children; i++){
+        wait(NULL);
+    }
+
+    printf("All child processes have finished. Parent (PID: %d) is displaying the final message.\n", getpid());
 
     return 0;
 }
